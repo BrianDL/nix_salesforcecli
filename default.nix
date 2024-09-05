206b218ddd4a2b36158a846bc6afe813e1f6f753
BrianDL <BrianDL@users.noreply.github.com>
2024-09-05 13:27:21 -0600

    Update default.nix

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable/sf-linux-x64.tar.xz";
-    sha256 = "sha256-VX4krCUpPEmm0Vxb1MhinAWD75DkK6NzO6CSkqQddIs="; 
+    sha256 = "sha256-HOBzzGuoqtqYi+VO0X432o+kN2M3MrOqwFiJumSIW8s="; 
   };
 
   nativeBuildInputs = [ stdenv.cc ];
